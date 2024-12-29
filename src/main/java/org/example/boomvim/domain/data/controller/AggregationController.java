41c6b0ce6158653c016ab9d731a63efbe8df991d
yusung103 <gimyuseong742@gmail.com>
2024-12-29 23:41:45 +0900

    add services

--- src/main/java/org/example/boomvim/domain/data/controller/AggregationController.java
+++ src/main/java/org/example/boomvim/domain/data/controller/AggregationController.java
@@ -2,7 +2,9 @@ package org.example.boomvim.domain.data.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.boomvim.domain.data.entity.Aggregation;
-import org.example.boomvim.domain.data.service.GetAggregationService;
+import org.example.boomvim.domain.data.service.Get6HourAggService;
+import org.example.boomvim.domain.data.service.GetAnHourAggService;
+import org.example.boomvim.domain.data.service.GetDayAggService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,25 @@ import java.util.List;
 @RequestMapping("/BoomVim")
 @RequiredArgsConstructor
 public class AggregationController {
-    private final GetAggregationService getAggregationService;
+    private final GetAnHourAggService getAggService;
+    private final Get6HourAggService get6HourAggService;
+    private final GetDayAggService getDayAggService;
 
     @GetMapping("/anHour")
     public ResponseEntity<List<Aggregation>> getLastHourData() {
-        List<Aggregation> getRes = getAggregationService.execute();
+        List<Aggregation> getRes = getAggService.execute();
         return ResponseEntity.ok(getRes);
+    }
+
+    @GetMapping("/sixHour")
+    public ResponseEntity<List<Aggregation>> get6HourData() {
+        List<Aggregation> get6Res = get6HourAggService.execute();
+        return ResponseEntity.ok(get6Res);
+    }
+
+    @GetMapping("/Day")
+    public ResponseEntity<List<Aggregation>> getDayData() {
+        List<Aggregation> getDayRes = getDayAggService.execute();
+        return ResponseEntity.ok(getDayRes);
     }
 }
